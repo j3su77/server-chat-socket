c2e397df75729cedcaf5517d1111922ee27684e3
j3su11 <jesusvergara44@gmail.com>
2022-05-04 00:08:28 -0500

    port changed 8080

--- config/default.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    corsOrigin: "http://localhost:3000",
-    port: 4000,
-    host: "localhost"
-};
