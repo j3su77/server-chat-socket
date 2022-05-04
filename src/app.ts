d3080574bc1723d8593f9c700fff242f5e00aecd
j3su11 <jesusvergara44@gmail.com>
2022-05-04 01:34:30 -0500

    cors update * 7

--- src/app.ts
+++ src/app.ts
@@ -19,14 +19,13 @@ app.use(cors({
   origin: "*",
   credentials: false
 }))
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*'); //<--
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers",
-'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json,Authorization');
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
-
 
 const io = new Server(httpServer, {
     cors: {
